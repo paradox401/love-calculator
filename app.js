0a51cbd26bf19dc13f1c5a25490b866010d21474
paradox401 <sandeshsapkota371@gmail.com>
2023-02-28 15:55:14 +0545

    anuja add

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let yourName = document.querySelector("#yourName");
 let theirName = document.querySelector("#theirName");
 let output = document.querySelector("#output");
 let singlePeoples = ['bikah', 'dinesh', "roshan", 'prajjwal', 'shreejan', 'kushal', 'bishal', 'mandal', 'manish']
+let anuja = ['anuja']
 // let gayMeter = 1;
 
 function calculate() {
@@ -29,12 +30,18 @@ function calculate() {
             output.innerText = "the given person is gay";
         }
         else {
-            let lovePercent = parseInt(Math.random() * 100);
-            output.innerText = name1 + " and " + name2 + " are " + lovePercent + " % in love";
 
+            if ((anuja.includes(name1.toLowerCase() || anuja.includes(name2.toLowerCase())))) {
+                output.innerText = "this person is not intrested";
+            }
+
+            else {
+                let lovePercent = parseInt(Math.random() * 100);
+                output.innerText = name1 + " and " + name2 + " are " + lovePercent + " % in love";
+
+            }
+
+            yourName.value = "";
+            theirName.value = "";
         }
-
-        yourName.value = "";
-        theirName.value = "";
     }
-}
